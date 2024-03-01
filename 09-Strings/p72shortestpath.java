89d1627bbb2efa3cd9563a95e9ed3b33c362e2a3
Madhav Hirani <madhavhirani2422@gmail.com>
2024-03-01 13:15:43 +0530

    Shortest Path

---
+++ 09-Strings/p72shortestpath.java
@@ -0,0 +1,45 @@
+public class p72shortestpath {
+
+    public static float shortestPath(String path)
+    {
+        int x=0,y=0;
+    
+        for(int i=0;i<path.length();i++)
+        {
+            char dir=path.charAt(i);
+
+            // North
+            if(dir == 'N')
+            {
+                y++;
+            }
+            // South
+
+            else if(dir=='S')
+            {
+                y--;
+            }
+
+            // East
+            else if(dir == 'E')
+            {
+                x++;
+            }
+
+            // West
+            else
+            {
+                x--;
+            }
+
+        }
+        int X2=x*x;
+        int Y2=y*y;
+        return (float) Math.sqrt(X2 + Y2);
+}
+
+    public static void main(String[] args) {
+        String str="WNEENESENNN";
+        System.out.println(shortestPath(str));
+    }
+}
