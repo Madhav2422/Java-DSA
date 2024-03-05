70be73595c9def2cffdeea042c94c31847848a34
Madhav Hirani <madhavhirani2422@gmail.com>
2024-03-06 02:03:20 +0530

    String Builder

---
+++ 09-Strings/p76stringbuilder.java
@@ -0,0 +1,11 @@
+public class p76stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("");
+        for(char ch='a';ch<='z';ch++)
+        {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+
+    }
+}
