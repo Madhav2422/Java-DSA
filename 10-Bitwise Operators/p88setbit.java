d9e0326e05d80b9864cab4aceaab009a659fac7b
Madhav Hirani <madhavhirani2422@gmail.com>
2024-03-15 21:45:46 +0530

    Set the ith bit

---
+++ 10-Bitwise Operators/p88setbit.java
@@ -0,0 +1,11 @@
+public class p88setbit {
+
+    public static int setbit(int n,int i){
+        int bitMask=1<<i;
+        return n|bitMask;
+       
+    }
+    public static void main(String[] args) {
+        System.out.println(setbit(10, 2));
+    }
+}
