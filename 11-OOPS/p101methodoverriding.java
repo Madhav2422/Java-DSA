611f094a97ff24c964cc60c2dcbbe16df9924b84
Madhav Hirani <madhavhirani2422@gmail.com>
2024-04-15 08:19:32 +0530

    Method Overriding

---
+++ 11-OOPS/p101methodoverriding.java
@@ -0,0 +1,18 @@
+public class p101methodoverriding {
+    public static void main(String[] args) {
+        Deer d1=new Deer();
+        d1.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("It eats everything");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats grass");
+    }
+}
